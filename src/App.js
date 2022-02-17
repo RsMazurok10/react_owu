c13a3f048ea43331d769b83ffe44080c0da97ee9
RsMazurok10 <mazurokrom@gmail.com>
2022-02-17 12:18:49 +0200

    hw_4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import {useReducer} from "react";
+import css from './Count.module.css'
+
+
+const reducer = (state, action) => {
+    if (action.counts === 3) {
+        if (action.type == 'inc') {
+            return {...state, count1: state.count1 + 1}
+        }
+        if (action.type == 'dec') {
+            return {...state, count1: state.count1 - 1}
+        }
+        if (action.type == 'res') {
+            return {...state, count1: state.count1 = 0}
+        } else {
+            return ('error')
+        }
+
+    }
+    if (action.counts === 2) {
+        if (action.type == 'inc') {
+            return {...state, count2: state.count2 + 1}
+        }
+        if (action.type == 'dec') {
+            return {...state, count2: state.count2 - 1}
+        }
+        if (action.type == 'res') {
+            return {...state, count2: state.count2 = 0}
+        } else {
+            return ('error')
+        }
+    }
+    if (action.counts === 1) {
+        if (action.type == 'inc') {
+            return {...state, count3: state.count3 + 1}
+        }
+        if (action.type == 'dec') {
+            return {...state, count3: state.count3 - 1}
+        }
+        if (action.type == 'res') {
+            return {...state, count3: state.count3 = 0}
+        } else {
+            return ('error')
+        }
+    }
+    return state;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [state, dispatch] = useReducer(reducer, {count1: 0, count2: 0, count3: 0});
+
+    return (
+        <div className={css.wrap}>
+            <div className={css.counter}>
+                <div>First counter: {state.count1}</div>
+                <div>
+                    <button onClick={() => dispatch({type: 'inc', counts: 3})}>INC</button>
+                    <button onClick={() => dispatch({type: 'dec', counts: 3})}>DEC</button>
+                    <button onClick={() => dispatch({type: 'res', counts: 3})}>RES</button>
+                </div>
+            </div>
+            <div className={css.counter}>
+                <div>Second counter: {state.count2}</div>
+                <div>
+                    <button onClick={() => dispatch({type: 'inc', counts: 2})}>INC</button>
+                    <button onClick={() => dispatch({type: 'dec', counts: 2})}>DEC</button>
+                    <button onClick={() => dispatch({type: 'res', counts: 2})}>RES</button>
+                </div>
+            </div>
+            <div className={css.counter}>
+                <div>Third counter: {state.count3}</div>
+                <div>
+                    <button onClick={() => dispatch({type: 'inc', counts: 1})}>INC</button>
+                    <button onClick={() => dispatch({type: 'dec', counts: 1})}>DEC</button>
+                    <button onClick={() => dispatch({type: 'res', counts: 1})}>RES</button>
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
